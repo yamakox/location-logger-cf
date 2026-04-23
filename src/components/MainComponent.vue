11b0e83d959438d148279ecd3bb23be952361d55
yamakox <yamakox@gmail.com>
2026-04-23 14:57:11 +0900

    prettierでフォーマットされてしまったので、oxfmtでフォーマットをやり直した。

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -10,10 +10,8 @@
         :longitude="137.1853999"
         :zoom="5"
       />
-      <div
-        class="button-area d-flex flex-row justify-content-between align-items-center"
-      >
-        <div class="d-flex flex-row justify-content-start align-items-center">
+      <div class="button-area d-flex justify-content-between align-items-center flex-row">
+        <div class="d-flex justify-content-start align-items-center flex-row">
           <button
             @click="onLocateButtonClick"
             class="btn btn-primary m-0 px-4 py-1 text-nowrap"
@@ -29,12 +27,9 @@
             記録
           </button>
         </div>
-        <div class="d-flex flex-row justify-content-end align-items-center">
+        <div class="d-flex justify-content-end align-items-center flex-row">
           <div class="mx-3 my-0 p-0 text-nowrap">
-            <label
-              class="hide-on-small-screen form-check-label text-nowrap"
-              for="trackButton"
-            >
+            <label class="hide-on-small-screen form-check-label text-nowrap" for="trackButton">
               自動追跡
             </label>
           </div>
@@ -72,9 +67,7 @@
           </template>
         </data-table>
       </div>
-      <div
-        class="footer-area d-flex flex-column justify-content-start align-items-center"
-      >
+      <div class="footer-area d-flex flex-column justify-content-start align-items-center">
         <div class="mx-0 mb-2">
           <a href="https://github.com/yamakox/location-logger-cf">本アプリ</a
           >はデモ用として運用しています。<br />
@@ -91,197 +84,193 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-import MapComponent from "./MapComponent.vue";
-import DataTable from "datatables.net-vue3";
-import DataTablesCore from "datatables.net";
-import "datatables.net-select";
-import axios from "axios";
+import { ref, onMounted } from 'vue'
+import MapComponent from './MapComponent.vue'
+import DataTable from 'datatables.net-vue3'
+import DataTablesCore from 'datatables.net'
+import 'datatables.net-select'
+import axios from 'axios'
 
 // DataTableの初期化
-DataTable.use(DataTablesCore);
+DataTable.use(DataTablesCore)
 const columns = [
-  { data: "timestamp", title: "記録日時", type: "date" },
-  { data: "address", title: "地名" },
-];
+  { data: 'timestamp', title: '記録日時', type: 'date' },
+  { data: 'address', title: '地名' },
+]
 const options = {
   paging: false,
   searching: false,
-  select: "single",
-  order: { name: "timestamp", dir: "desc" },
-};
+  select: 'single',
+  order: { name: 'timestamp', dir: 'desc' },
+}
 
 // 位置情報の一覧(最大300件)
-const logs = ref([]);
-const maxLogCount = 300;
+const logs = ref([])
+const maxLogCount = 300
 
 // map-componentへのref
-const map = ref();
+const map = ref()
 
 // 自動追跡用チェックボックスへのref
-const trackButtonChecked = ref(false);
+const trackButtonChecked = ref(false)
 
 // location-foundイベント発生時のイベントオブジェクトへのref
-const locationFoundEvent = ref(null);
+const locationFoundEvent = ref(null)
 
 // Cookieのcidの値
-const cid = ref("-----");
-const version = ref("-----");
+const cid = ref('-----')
+const version = ref('-----')
 
 // 最後に選択した履歴の日付の午前0時のtimestamp
-let lastTimestamp0 = null;
+let lastTimestamp0 = null
 
 // 日時書式
 const dateFormat = {
-  year: "numeric",
-  month: "2-digit",
-  day: "2-digit",
-};
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+}
 const timeFormat = {
-  hour: "2-digit",
-  minute: "2-digit",
-};
+  hour: '2-digit',
+  minute: '2-digit',
+}
 
 // timestampから日時文字列の取得
 function getTimeString(timestamp) {
-  let date = new Date(timestamp);
-  return (
-    date.toLocaleDateString([], dateFormat) +
-    " " +
-    date.toLocaleTimeString([], timeFormat)
-  );
+  let date = new Date(timestamp)
+  return date.toLocaleDateString([], dateFormat) + ' ' + date.toLocaleTimeString([], timeFormat)
 }
 
 // timestampからその日の午前0時のtimestampの算出
 function getTimestamp0(timestamp) {
-  let ds = new Date(timestamp).toLocaleDateString();
-  return Date.parse(ds);
+  let ds = new Date(timestamp).toLocaleDateString()
+  return Date.parse(ds)
 }
 
 // 位置情報の履歴を地図上に表示する
 function setLatLngs(timestamp) {
-  let timestamp0 = getTimestamp0(timestamp);
-  let latlngs = [];
+  let timestamp0 = getTimestamp0(timestamp)
+  let latlngs = []
   if (timestamp != null) {
-    let timestamp24 = timestamp0 + 24 * 3600 * 1000;
+    let timestamp24 = timestamp0 + 24 * 3600 * 1000
     logs.value
       .filter((x) => timestamp0 <= x.timestamp && x.timestamp < timestamp24)
       .forEach((x) => {
-        latlngs.push([x.latitude, x.longitude]);
-      });
+        latlngs.push([x.latitude, x.longitude])
+      })
   } else {
     logs.value.forEach((record) => {
-      latlngs.push([record.latitude, record.longitude]);
-    });
+      latlngs.push([record.latitude, record.longitude])
+    })
   }
-  map.value.setLatLngs(latlngs);
+  map.value.setLatLngs(latlngs)
   if (lastTimestamp0 != timestamp0) {
-    map.value.fitLatLngs();
-    lastTimestamp0 = timestamp0;
+    map.value.fitLatLngs()
+    lastTimestamp0 = timestamp0
   }
 }
 
 // map-componentのイベントハンドラ
 function onLocationFound(event) {
-  locationFoundEvent.value = event;
+  locationFoundEvent.value = event
   if (trackButtonChecked.value) {
-    postLocation();
+    postLocation()
   }
 }
 
 // 位置情報の履歴のイベントハンドラ
 function showLocation(record) {
-  setLatLngs(record.timestamp);
+  setLatLngs(record.timestamp)
   map.value.showLocation(
     record.latitude,
     record.longitude,
     record.timestamp,
     record.address,
-    record.distance,
-  );
+    record.distance
+  )
 }
 
 function showLocation2(e, dt, type, index) {
-  showLocation(logs.value[index]);
-  dt.rows().deselect();
+  showLocation(logs.value[index])
+  dt.rows().deselect()
 }
 
 // ボタンのイベントハンドラ
 function onLocateButtonClick(event) {
-  map.value.locate();
+  map.value.locate()
 }
 
 function onTrackLocateButtonChange(event) {
   if (trackButtonChecked.value) {
-    console.log("onTrackLocateButtonChange: true");
-    map.value.locate(true);
+    console.log('onTrackLocateButtonChange: true')
+    map.value.locate(true)
   } else {
-    console.log("onTrackLocateButtonChange: false");
-    map.value.stopLocate();
+    console.log('onTrackLocateButtonChange: false')
+    map.value.stopLocate()
   }
 }
 
 async function onRecordButtonClick(event) {
-  await postLocation();
+  await postLocation()
 }
 
 function pushLog(record) {
-  logs.value.unshift(record);
-  window.scrollTo({ top: 0, behavior: "instant" });
-  setLatLngs(record.timestamp);
+  logs.value.unshift(record)
+  window.scrollTo({ top: 0, behavior: 'instant' })
+  setLatLngs(record.timestamp)
 }
 
 async function postLocation() {
-  let e = locationFoundEvent.value;
-  locationFoundEvent.value = null;
+  let e = locationFoundEvent.value
+  locationFoundEvent.value = null
   let record = {
     timestamp: e.timestamp,
     latitude: e.latlng.lat,
     longitude: e.latlng.lng,
     address: e.address,
     distance: e.distance,
-  };
+  }
 
   // `withCredentials: true`を指定すると、Cookieのcidの値を取得できる
   try {
-    await axios.post(`/api/v1/location`, record, { withCredentials: true });
+    await axios.post(`/api/v1/location`, record, { withCredentials: true })
     if (logs.value.length >= maxLogCount) {
-      logs.value.length = maxLogCount - 1;
+      logs.value.length = maxLogCount - 1
     }
-    pushLog(record);
+    pushLog(record)
   } catch (error) {
-    locationFoundEvent.value = e;
-    console.log(error);
-    record.address = `${record.address}: 送信失敗`;
-    pushLog(record);
+    locationFoundEvent.value = e
+    console.log(error)
+    record.address = `${record.address}: 送信失敗`
+    pushLog(record)
   }
 }
 
 // appマウント時の処理
 onMounted(async () => {
-  console.log("MainComponent.onMounted");
+  console.log('MainComponent.onMounted')
   try {
     // バージョン情報の取得
     const resVersion = await axios.get(`/api/v1/misc/version`, {
       withCredentials: true,
-    });
-    version.value = resVersion.data?.version ?? "-----";
+    })
+    version.value = resVersion.data?.version ?? '-----'
 
     // CIDの取得
     const resClient = await axios.get(`/api/v1/client`, {
       withCredentials: true,
-    });
-    cid.value = resClient.data?.cid ?? "-----";
+    })
+    cid.value = resClient.data?.cid ?? '-----'
 
     // 位置情報の一覧の取得
     if (true) {
       const resLocation = await axios.get(`/api/v1/location`, {
         withCredentials: true,
-      });
-      logs.value = resLocation.data;
-      setLatLngs(null);
+      })
+      logs.value = resLocation.data
+      setLatLngs(null)
       if (logs.value.length > 1) {
-        map.value.fitLatLngs();
+        map.value.fitLatLngs()
       }
     } else {
       // for debugging
@@ -290,24 +279,24 @@ onMounted(async () => {
           timestamp: new Date().getTime(),
           latitude: 35.6811124,
           longitude: 139.764516,
-          address: "東京駅",
+          address: '東京駅',
         },
         {
           timestamp: new Date().getTime() + 60000,
           latitude: 36.5780486,
           longitude: 136.6455965,
-          address: "金沢駅",
+          address: '金沢駅',
         },
-      ];
-      setLatLngs(null);
+      ]
+      setLatLngs(null)
       if (logs.value.length > 1) {
-        map.value.fitLatLngs();
+        map.value.fitLatLngs()
       }
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-});
+})
 </script>
 
 <style scoped>
--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -6,17 +6,17 @@
 </template>
 
 <script setup>
-import { ref, onMounted, watch } from "vue";
-import L from "leaflet";
-import { openReverseGeocoder } from "https://cdn.skypack.dev/@geolonia/open-reverse-geocoder@latest";
-import { Geodesic } from "geographiclib-geodesic";
-import NoSleep from "nosleep.js";
+import { ref, onMounted, watch } from 'vue'
+import L from 'leaflet'
+import { openReverseGeocoder } from 'https://cdn.skypack.dev/@geolonia/open-reverse-geocoder@latest'
+import { Geodesic } from 'geographiclib-geodesic'
+import NoSleep from 'nosleep.js'
 
 // スリープ抑止オブジェクト
-const noSleep = new NoSleep();
+const noSleep = new NoSleep()
 
 // GeographicLibの2点間の距離を算出するオブジェクトの取得
-const geod = Geodesic.WGS84;
+const geod = Geodesic.WGS84
 
 // locate(auto=true)時の状態を格納するオブジェクト
 const autoModeState = {
@@ -26,214 +26,194 @@ const autoModeState = {
   lastUpdateTime: null,
   minInterval: 60000, // 単位: ミリ秒
   minDistance: 30, // 単位: m(メートル)
-};
+}
 
 // 定数
-const defaultZoom = 15;
+const defaultZoom = 15
 
 // プロパティの定義
 const props = defineProps({
   latitude: { type: Number, default: 0.0 },
   longitude: { type: Number, default: 0.0 },
   zoom: { type: Number, default: 1 },
-});
+})
 
 // このコンポーネントから送出するイベントの定義
-const emit = defineEmits(["locationFound"]);
+const emit = defineEmits(['locationFound'])
 
 // mapオブジェクトを割り当てるdiv要素(名前はref属性の値と一致させる)
-const mapRef = ref();
+const mapRef = ref()
 
 // 点滅div要素
-const blinker = ref();
+const blinker = ref()
 
 // Leafletのmapオブジェクト
-let map = null;
+let map = null
 
 // leafletのpolylineオブジェクト
-let pathLine = null;
+let pathLine = null
 
 // Geolocation APIを使ってユーザーの位置を更新
 function locate(auto = false) {
   if (map === null || autoModeState.auto) {
-    return;
+    return
   }
-  console.log("locate");
+  console.log('locate')
   if (auto) {
-    autoModeState.auto = true;
-    autoModeState.lastLatitude = null;
-    autoModeState.lastLongitude = null;
-    autoModeState.lastUpdateTime = null;
-    blinker.value.style.backgroundColor = "transparent";
-    noSleep.enable();
+    autoModeState.auto = true
+    autoModeState.lastLatitude = null
+    autoModeState.lastLongitude = null
+    autoModeState.lastUpdateTime = null
+    blinker.value.style.backgroundColor = 'transparent'
+    noSleep.enable()
   }
   map.locate({
     watch: auto,
     setView: !auto, // autoの場合はビューを自動更新しない
     maxZoom: defaultZoom,
     enableHighAccuracy: true,
-  });
+  })
 }
 
 // Geolocation APIの自動更新を停止
 function stopLocate() {
-  map.stopLocate();
-  noSleep.disable();
-  autoModeState.auto = false;
-  blinker.value.style.backgroundColor = "transparent";
+  map.stopLocate()
+  noSleep.disable()
+  autoModeState.auto = false
+  blinker.value.style.backgroundColor = 'transparent'
 }
 
 // 指定された緯度経度の場所を表示
 function showLocation(latitude, longitude, timestamp, address, distance) {
-  map.setView([latitude, longitude], map.zoom);
-  popupMessage(latitude, longitude, timestamp, address, distance);
+  map.setView([latitude, longitude], map.zoom)
+  popupMessage(latitude, longitude, timestamp, address, distance)
 }
 
 // 位置情報の履歴に合わせて地図を表示
 function setLatLngs(latlngs) {
-  pathLine.setLatLngs(latlngs);
+  pathLine.setLatLngs(latlngs)
 }
 
 // 位置情報の履歴に表示範囲を合わせる
 function fitLatLngs() {
-  let bounds = pathLine.getBounds();
+  let bounds = pathLine.getBounds()
   if (bounds.isValid()) {
-    map.fitBounds(bounds);
+    map.fitBounds(bounds)
   }
 }
 
 // プロパティの設定値に則って地図上の位置を更新
 function updateLocation(latitude, longitude, zoom) {
   if (map === null) {
-    return;
+    return
   }
-  console.log(`[updateLocation] ${latitude}, ${longitude}, ${zoom}`);
-  map.setView([latitude, longitude], zoom);
+  console.log(`[updateLocation] ${latitude}, ${longitude}, ${zoom}`)
+  map.setView([latitude, longitude], zoom)
 }
 
 // 緯度経度の場所にメッセージをポップアップ表示する
-function popupMessage(
-  latitude,
-  longitude,
-  timestamp,
-  address,
-  distance = null,
-) {
+function popupMessage(latitude, longitude, timestamp, address, distance = null) {
   // 日時文字列の取得
-  let date = new Date(timestamp);
-  let localTime =
-    date.toLocaleDateString() + " " + date.toLocaleTimeString().slice(0, -3);
+  let date = new Date(timestamp)
+  let localTime = date.toLocaleDateString() + ' ' + date.toLocaleTimeString().slice(0, -3)
 
   // 日時と住所のポップアップ表示
-  let distMessage = "";
+  let distMessage = ''
   if (distance != null) {
-    distMessage = `<br>前回から${distance}m移動`;
+    distMessage = `<br>前回から${distance}m移動`
   }
-  let message = `<div style="text-align: center;">${localTime}<br>${address}${distMessage}</div>`;
-  L.popup().setLatLng([latitude, longitude]).setContent(message).openOn(map);
+  let message = `<div style="text-align: center;">${localTime}<br>${address}${distMessage}</div>`
+  L.popup().setLatLng([latitude, longitude]).setContent(message).openOn(map)
 }
 
 // 緯度経度の場所に住所をポップアップ表示してイベント通知する
 function notifyAddress(event, address) {
   // 日時と住所のポップアップ表示
-  popupMessage(
-    event.latlng.lat,
-    event.latlng.lng,
-    event.timestamp,
-    address,
-    event.distance,
-  );
+  popupMessage(event.latlng.lat, event.latlng.lng, event.timestamp, address, event.distance)
 
   // イベントを親コンポーネントに送出する
-  event.address = address;
-  emit("locationFound", event);
+  event.address = address
+  emit('locationFound', event)
 }
 
 // イベントハンドラ
 function onLocationFound(event) {
-  let distance = null;
-  let currentTime = new Date().getTime();
+  let distance = null
+  let currentTime = new Date().getTime()
   if (autoModeState.auto) {
-    let bgcol = blinker.value.style.backgroundColor;
-    blinker.value.style.backgroundColor =
-      bgcol != "red" ? "red" : "transparent";
+    let bgcol = blinker.value.style.backgroundColor
+    blinker.value.style.backgroundColor = bgcol != 'red' ? 'red' : 'transparent'
     if (autoModeState.lastUpdateTime != null) {
-      if (
-        currentTime - autoModeState.lastUpdateTime <
-        autoModeState.minInterval
-      ) {
-        return;
+      if (currentTime - autoModeState.lastUpdateTime < autoModeState.minInterval) {
+        return
       } else {
         let r = geod.Inverse(
           autoModeState.lastLatitude,
           autoModeState.lastLongitude,
           event.latlng.lat,
-          event.latlng.lng,
-        );
-        distance = r.s12.toFixed(2);
+          event.latlng.lng
+        )
+        distance = r.s12.toFixed(2)
         if (distance < autoModeState.minDistance) {
-          return;
+          return
         }
       }
     }
-    autoModeState.lastLatitude = event.latlng.lat;
-    autoModeState.lastLongitude = event.latlng.lng;
-    autoModeState.lastUpdateTime = currentTime;
+    autoModeState.lastLatitude = event.latlng.lat
+    autoModeState.lastLongitude = event.latlng.lng
+    autoModeState.lastUpdateTime = currentTime
   }
-  event.distance = distance;
-  event.timestamp = currentTime; // macOS Safariではtimestampの値が不正なので、上書きする
+  event.distance = distance
+  event.timestamp = currentTime // macOS Safariではtimestampの値が不正なので、上書きする
 
   console.log(
-    `onLocationFound: ${event.latlng.lat}, ${event.latlng.lng} (distance= ${event.distance} m)`,
-  );
+    `onLocationFound: ${event.latlng.lat}, ${event.latlng.lng} (distance= ${event.distance} m)`
+  )
 
   // 緯度経度から都道府県・市区町村を検索 (経度・緯度で渡す必要がある)
   openReverseGeocoder([event.latlng.lng, event.latlng.lat])
     .then((result) => {
       // resultサンプル {"code": "13101", "prefecture": "東京都", "city": "千代田区"}
-      notifyAddress(event, result.prefecture + result.city);
+      notifyAddress(event, result.prefecture + result.city)
     })
     .catch((error) => {
-      notifyAddress(event, "住所不明");
-    });
+      notifyAddress(event, '住所不明')
+    })
 }
 
 function onLocationError(event) {
   if (autoModeState.auto) {
-    stopLocate();
+    stopLocate()
   }
-  var msg = `Location Error (${event.code}): ${event.message}`;
-  console.log(msg);
-  alert(msg);
+  var msg = `Location Error (${event.code}): ${event.message}`
+  console.log(msg)
+  alert(msg)
 }
 
 // appマウント時の処理
 onMounted(() => {
-  console.log("MapComponent.onMounted");
-  map = L.map(mapRef.value);
-  pathLine = L.polyline([], { color: "blue" }).addTo(map);
-  map.on("locationfound", onLocationFound);
-  map.on("locationerror", onLocationError);
-  updateLocation(props.latitude, props.longitude, props.zoom);
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  console.log('MapComponent.onMounted')
+  map = L.map(mapRef.value)
+  pathLine = L.polyline([], { color: 'blue' }).addTo(map)
+  map.on('locationfound', onLocationFound)
+  map.on('locationerror', onLocationError)
+  updateLocation(props.latitude, props.longitude, props.zoom)
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' +
       ' | &copy; <a href="https://github.com/geolonia/open-reverse-geocoder">geolonia</a>',
-  }).addTo(map);
-});
+  }).addTo(map)
+})
 
 // プロパティの監視
 watch(
   [() => props.latitude, () => props.longitude, () => props.zoom],
-  (
-    [newLatitude, newLongitude, newZoom],
-    [oldLatitude, oldLongitude, oldZoom],
-  ) => {
-    console.log("[watch] props");
-    updateLocation(newLatitude, newLongitude, newZoom);
-  },
-);
+  ([newLatitude, newLongitude, newZoom], [oldLatitude, oldLongitude, oldZoom]) => {
+    console.log('[watch] props')
+    updateLocation(newLatitude, newLongitude, newZoom)
+  }
+)
 
 // メソッドの公開
 defineExpose({
@@ -242,7 +222,7 @@ defineExpose({
   showLocation,
   setLatLngs,
   fitLatLngs,
-});
+})
 </script>
 
 <style scoped>
